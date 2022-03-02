b326f87f2c3f947c35cbd39c26dc56b75b90ce06
Kevin <kevin.brun.dev@gmail.com>
2022-03-02 16:50:36 +0100

    Accessibilité produits
    Produits accessibles avec api_token
    non accessible sans

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('product', 'ProductController');
+Route::middleware('auth:api')->group(function(){
+    Route::apiResource('product', 'ProductController');
+});
+
